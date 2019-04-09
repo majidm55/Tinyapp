fb1b3a75400e483b91ab13f089014b9484992e51
Majid <mohammadmajid55@gmail.com>
2019-04-09 23:11:42 +0000

    add long short url

--- express_server.js
+++ express_server.js
@@ -24,7 +24,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index",templateVars);
 });
 
-
+app.get("/urls/:shortURL", (req, res) => {
+  var short = req.params.shortURL;
+  let templateVars = { shortURL: short,
+                      longURL: urlDatabase[short] };
+  res.render("urls_show", templateVars);
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
