ba8703c3cef221cbc6f16d5461d2f295bf077070
Majid <mohammadmajid55@gmail.com>
2019-05-03 22:35:14 +0000

    fixed edit link for shorturl and added register and login links

--- express_server.js
+++ express_server.js
@@ -1,4 +1,3 @@
-
 //variables and requirements//
 var cookieSession = require('cookie-session');
 var express = require("express");
@@ -12,7 +11,9 @@ app.use(cookieSession({
   keys: ["id", "user_id"]
 }))
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.set("view engine", "ejs");
 
@@ -20,23 +21,27 @@ app.set("view engine", "ejs");
 
 //user database//
 var urlDatabase = {
-  b6UTxQ: { longURL: "https://www.tsn.ca",
-            userID: "userRandomID" },
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "userRandomID"
+  },
 
-  i3BoGr: { longURL: "https://www.google.ca",
-            userID: "user2RandomID" }
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "user2RandomID"
+  }
 };
 
 var users = {
   "userRandomID": {
     id: "userRandomID",
     email: "abc@d.com",
-    password:bcrypt.hashSync("1234", 10),
+    password: bcrypt.hashSync("1234", 10),
   },
- "user2RandomID": {
+  "user2RandomID": {
     id: "user2RandomID",
     email: "abc@e.com",
-    password:bcrypt.hashSync("12345", 10)
+    password: bcrypt.hashSync("12345", 10)
 
   }
 }
@@ -48,23 +53,23 @@ function generateRandomString() {
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
   for (let i = 0; i < length; i++)
-  randString += possible[Math.floor(Math.random() * possible.length)];
+    randString += possible[Math.floor(Math.random() * possible.length)];
   return randString;
 }
 // user match function//
-function userMatch (email,password) {
+function userMatch(email, password) {
   for (let id in users) {
 
     if (users[id].email === email) {
       if (bcrypt.compareSync(password, users[id].password)) {
-        return users[id] ;
+        return users[id];
       }
     }
   }
   return false;
 }
 //finding url in the database//
-function urlsForUser (id) {
+function urlsForUser(id) {
   let urls = {};
   for (shortURL in urlDatabase) {
     if (urlDatabase[shortURL].userID === id) {
@@ -85,43 +90,43 @@ app.get("/", (req, res) => {
 });
 
 //route for going to homepage using "/urls"  //
-app.get("/urls", (req, res) => {                ///user id cookies
+app.get("/urls", (req, res) => { ///user id cookies
   let user_id = req.session.user_id
-    if (!user_id){
-      res.redirect("/login");
-    } else {
+  if (!user_id) {
+    res.redirect("/login");
+  } else {
     let templateVars = {
-     'user_id' : user_id,
-     'urls': urlsForUser(user_id),
-     'email': (users[user_id] ? users[user_id].email: users[user_id])
-     };
-      res.render("urls_index",templateVars);
+      'user_id': user_id,
+      'urls': urlsForUser(user_id),
+      'email': (users[user_id] ? users[user_id].email : users[user_id])
     };
+    res.render("urls_index", templateVars);
+  };
 });
 
 // route to add new url//
 app.get("/urls/new", (req, res) => {
   let user_id = req.session.user_id
-    if (!user_id){
-      res.redirect("/login");
-    } else {
+  if (!user_id) {
+    res.redirect("/login");
+  } else {
     let templateVars = {
-     'user_id' : user_id,
-     'urls': urlsForUser(user_id),
-     'email': (users[user_id] ? users[user_id].email: users[user_id])
-     };
-      res.render("urls_new",templateVars);
+      'user_id': user_id,
+      'urls': urlsForUser(user_id),
+      'email': (users[user_id] ? users[user_id].email : users[user_id])
     };
+    res.render("urls_new", templateVars);
+  };
 });
 
 // posting to url//
-app.post("/urls", function(req, res) {
+app.post("/urls", function (req, res) {
   let newURL = generateRandomString();
-    urlDatabase[newURL] = {
+  urlDatabase[newURL] = {
     longURL: req.body.longURL,
     userID: req.session.user_id
-    }
-    res.redirect(`/urls`);
+  }
+  res.redirect(`/urls`);
 });
 
 //delete url route//
@@ -131,16 +136,21 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
-// going to short URL//
-app.get("/urls/:id" , (req, res) => {
-  let short = req.params.id;
-  const longURL = urlDatabase[short];
-  let userID = req.session.user_id
-  let user = users[userID]
-  let templateVars = { user: user, shortURL:short };
-  res.render("urls_show",templateVars);
-});
+// going to edit short URL//
+app.get("/urls/:id", (req, res) => {
+  if (req.session.user_id) {
+    let templateVars = {
+      user_id: req.session.user_id,
+      shortURL: req.params.shortURL,
+      longURL: urlDatabase[req.params.shortURL],
+      email: (users[req.session.user_id] ? users[req.session.user_id].email : users[req.session.user_id])
+    };
+    res.render("urls_show", templateVars);
+  } else {
+    res.redirect("/login")
 
+  };
+});
 //posting a long url//
 app.post("/urls/:id", (req, res) => {
   let add = req.body.longURL;
@@ -149,16 +159,16 @@ app.post("/urls/:id", (req, res) => {
 });
 
 //logout route//
-app.post("/logout", (req,res) => {                     //Logout
+app.post("/logout", (req, res) => { //Logout
   req.session = null;
   res.redirect("/urls");
 });
 
 //login route takes you to main page//
-app.post("/login", (req,res) => {
+app.post("/login", (req, res) => {
   email = req.body.email;
   password = req.body.password;
-  let user = userMatch(email,password)
+  let user = userMatch(email, password)
   if (user) {
     let userID = user.id;
     req.session.user_id = userID;
@@ -167,35 +177,40 @@ app.post("/login", (req,res) => {
   res.status(403).send("Username or password incorrect");
 });
 
- //register route//
-app.post("/register", (req,res) => {
+//register route//
+app.post("/register", (req, res) => {
 
-  for(i in users){
-   if(users[i].email == req.body.email){ //checking if email already exists
-   res.status(400)
-   .send('email already exists');
-   }
- }
+  for (i in users) {
+    if (users[i].email == req.body.email) { //checking if email already exists
+      res.status(400)
+        .send('email already exists');
+    }
+  }
   const email = req.body.email;
   const password = req.body.password;
   const newUserID = generateRandomString();
   users[newUserID] = {};
 
-  if(email === "" || password === "") {
-   res.status(400)        // HTTP status 400: NotFound
-   res.send('Not found');}
+  if (email === "" || password === "") {
+    res.status(400) // HTTP status 400: NotFound
+    res.send('Not found');
+  } else {
+    const id = Object.keys(users).length + 1;
+    const {
+      email,
+      password
+    } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 10);
 
- else{ const id = Object.keys(users).length + 1;
-  const {email, password} = req.body;
-  const hashedPassword = bcrypt.hashSync(password, 10);
+    users[id] = {
+      id: id,
+      email: email,
+      password: hashedPassword
+    };
 
-  users[id] = { id : id,
-                     email : email,
-                     password : hashedPassword };
-
-  req.session.user_id = id;
-  res.redirect("/urls");
-}
+    req.session.user_id = id;
+    res.redirect("/urls");
+  }
 });
 
 //login page route//
@@ -204,7 +219,7 @@ app.get("/login", (req, res) => {
     user_id: req.session.user_id,
     email: (users[req.session.user_id] ? users[req.session.user_id].email : users[req.session.user_id])
   }
-  res.render("urls_login",templateVars);
+  res.render("urls_login", templateVars);
 });
 
 //regitser page get route//
@@ -212,15 +227,15 @@ app.get("/register", (req, res) => {
   let templateVars = {
     user_id: req.session.user_id,
   }
-  res.render("urls_register",templateVars);
+  res.render("urls_register", templateVars);
 });
 
 
 //redirecting to the corresponding long url using a short url//
 app.get("/u/:shortURL", (req, res) => {
-   let shortURL = req.params.shortURL;
-   let longURL = urlDatabase[shortURL].longURL;
-   if (longURL) {
+  let shortURL = req.params.shortURL;
+  let longURL = urlDatabase[shortURL].longURL;
+  if (longURL) {
     res.redirect(longURL);
   } else {
     res.send(shortURL, "is not a valid URL");
